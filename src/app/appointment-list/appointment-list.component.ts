5e4bed47b958f514e087d7e2eccbc2c00b30133f
Mohammad Najar <mmotieian@yahoo.com>
2023-11-17 21:24:32 -0600

    adding bootstrap and complete app

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,12 +1,17 @@
 import {Component} from '@angular/core';
 import {Appointment} from "../models/appointment";
+import {OnInit} from "@angular/core";
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit{
+  ngOnInit(): void {
+      // alert('Method not implemented.');
+    this.appointments = JSON.parse(localStorage.getItem('appointments') || '[]');
+  }
 
   newAppointmentTitle: string = '';
   newAppointmentDate: Date = new Date();
@@ -24,7 +29,13 @@ export class AppointmentListComponent {
       this.newAppointmentTitle = '';
       this.newAppointmentDate = new Date();
 
-      // alert(this.appointments.length);
+      localStorage.setItem('appointments', JSON.stringify(this.appointments));
+
     }
+  }
+
+  deleteAppointment(index: number) {
+    this.appointments.splice(index, 1);
+    localStorage.setItem('appointments', JSON.stringify(this.appointments));
   }
 }
